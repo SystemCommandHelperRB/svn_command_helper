2a54836937a26ac4d0ad59ec2716aca3e429af1f
Narazaka <info@narazaka.net>
2016-05-20 21:49:05 +0900

    yard

--- lib/svn_command_helper.rb
+++ lib/svn_command_helper.rb
@@ -3,11 +3,17 @@ require 'pathname'
 require 'yaml'
 require "system_command_helper"
 
+# Subversion command helper
 module SvnCommandHelper
+  # Subversion native command and some utilities
   module Svn
+    # module methods
     module ModuleMethods
       include ::SystemCommandHelper
 
+      # svn commit
+      # @param [String] message commit message
+      # @param [path string like] path target path
       def commit(message, path = ".")
         if cap("svn status #{path}").empty?
           sys "svn revert -R #{path}"
@@ -18,27 +24,47 @@ module SvnCommandHelper
         sys "svn update #{path}"
       end
 
+      # svn list
+      # @param [uri string like] uri target uri
+      # @param [Boolean] recursive --recursive
+      # @return [Array<String>] paths
       def list(uri, recursive = false)
         cap("svn list #{recursive ? '-R' : ''} #{uri}").split(/\n/).compact
           .reject {|path| path.empty?}
       end
 
+      # svn list --recursive
+      # @param [uri string like] uri target uri
+      # @return [Array<String>] paths
       def list_recursive(uri)
         list(uri, true)
       end
 
+      # svn list -> grep only files
+      # @param [uri string like] uri target uri
+      # @param [Boolean] recursive --recursive
+      # @return [Array<String>] file paths
       def list_files(uri, recursive = false)
         list(uri, recursive).reject {|path| path.end_with?("/")} # dir
       end
 
+      # svn list --recursive -> grep only files
+      # @param [uri string like] uri target uri
+      # @return [Array<String>] file paths
       def list_files_recursive(uri)
         list_files(uri, true)
       end
 
-      def update(path, depth = nil)
+      # svn update
+      # @param [path string like] path target path
+      # @param [depth] depth --set-depth
+      def update(path = ".", depth = nil)
         sys "svn update #{depth ? "--set-depth #{depth}" : ""} #{path}"
       end
 
+      # svn update to deep path recursive
+      # @param [path string like] path target path
+      # @param [depth] depth --set-depth for only new updated dirs
       def update_deep(path, depth = nil)
         root = Pathname.new(Svn.working_copy_root_path(path)).realpath
         end_path = Pathname.new(path.to_s).expand_path.realpath
@@ -55,18 +81,30 @@ module SvnCommandHelper
         end
       end
 
+      # svn info -> yaml parse
+      # @param [path string like] path target path
+      # @return [Hash<String, String>] svn info contents
       def info(path = ".")
         YAML.load(cap("svn info #{path}"))
       end
 
+      # svn cat
+      # @param [path string like] path target path
+      # @return [String] file contents
       def cat(path)
         cap("svn cat #{path}")
       end
 
+      # Working Copy Root Path from svn info
+      # @param [path string like] path target path
+      # @return [String] Working Copy Root Path
       def working_copy_root_path(path = ".")
         info(path)["Working Copy Root Path"]
       end
 
+      # find common part of the given uris
+      # @param [Array<uri string like>] uris target uri
+      # @return [String] common part of the given uris
       def base_uri_of(uris)
         uris.reduce(Pathname.new(uris.first.to_s)) do |base_uri, uri|
           rel = Pathname.new(uri).relative_path_from(base_uri)
@@ -75,6 +113,9 @@ module SvnCommandHelper
         end.to_s
       end
 
+      # copy single transaction
+      # @param [SvnFileCopyTransaction] transaction from and to info
+      # @param [String] message commit message
       def copy_single(transaction, message)
         transactions = transaction.glob_transactions
         raise "copy_single: #{transaction.from} not exists" if transactions.empty?
@@ -97,6 +138,9 @@ module SvnCommandHelper
         end
       end
 
+      # copy multi transactionS
+      # @param [Array<SvnFileCopyTransaction>] transactions from and to info list
+      # @param [String] message commit message
       def copy_multi(transactions, message)
         base_uri = base_uri_of(transactions.map(&:from_base) + trnsactions.map(&:to_base))
         transactions.each do |transaction|
@@ -118,16 +162,22 @@ module SvnCommandHelper
         end
       end
 
+      # check transaction from file exists
+      # @param [SvnFileCopyTransaction] transaction from and to info
+      # @param [Boolean] raise_if_from_not_found raise if from not found
+      # @return [Boolean] true if file exists
       def check_exists(transaction, raise_if_from_not_found = true)
         unless transaction.from_exist?
           if !raise_if_from_not_found
-            return
+            false
           elsif transaction.to_exist?
             puts "[WARNING] File:#{file}はコピー先のみにあります"
-            return
+            false
           else
             raise "[Error] File:#{file}が見つかりません！"
           end
+        else
+          true
         end
       end
     end
@@ -135,49 +185,76 @@ module SvnCommandHelper
     extend ModuleMethods
   end
 
+  # svn file copy transaction
+  # @attr [String] from_base from base uri
+  # @attr [String] to_base to base uri
+  # @attr [String] file file basename
   class SvnFileCopyTransaction
-    attr_reader :from_base, :to_base, :file
+    attr_reader :from_base
+    attr_reader :to_base
+    attr_reader :file
 
+    # @param [String] from_base from base uri
+    # @param [String] to_base to base uri
+    # @param [String] file file basename
     def initialize(from_base:, to_base:, file:)
       @from_base = from_base
       @to_base = to_base
       @file = file
     end
 
+    # from uri
+    # @return [String] from uri
     def from
       File.join(@from_base, @file)
     end
 
+    # to uri
+    # @return [String] to uri
     def to
       File.join(@to_base, @file)
     end
 
+    # filename glob (like "hoge*") to each single file transaction
+    # @return [Array<SvnFileCopyTransaction>] transactions
     def glob_transactions
       Svn.list_files(@from_base)
         .select{|_file| File.fnmatch(@file, _file)}
         .map{|_file| SvnFileCopyTransaction.new(from_base: @from_base, to_base: @to_base, file: _file)}
     end
 
+    # from uri exists?
+    # @return [Boolean]
     def from_exist?
       Svn.list_files(@from_base).find{|_file| File.fnmatch(@file, _file)}
     end
 
+    # to uri exists?
+    # @return [Boolean]
     def to_exist?
       Svn.list_files(@to_base).find{|_file| File.fnmatch(@file, _file)}
     end
 
+    # relative from base path from given base uri
+    # @return [String] relative from base path
     def relative_from_base(path)
       Pathname.new(@from_base).relative_path_from(Pathname.new(path)).to_s
     end
 
+    # relative to base path from given base uri
+    # @return [String] relative to base path
     def relative_to_base(path)
       Pathname.new(@to_base).relative_path_from(Pathname.new(path)).to_s
     end
 
+    # relative from path from given base uri
+    # @return [String] relative from path
     def relative_from(path)
       File.join(relative_from_base(path), @file)
     end
 
+    # relative to path from given base uri
+    # @return [String] relative to path
     def relative_to(path)
       File.join(relative_from_to(path), @file)
     end
